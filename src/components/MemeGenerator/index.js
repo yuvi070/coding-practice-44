05fc32e511080ecb6f988cd983af453f986e4fdc
yuvi070 <yuvarajchavan448@gmail.com>
2024-10-22 01:34:23 +0000

    version 1

---
+++ src/components/MemeGenerator/index.js
@@ -0,0 +1,120 @@
+import {Component} from 'react'
+
+import {
+  MainHeading,
+  SubHeading,
+  InputBox,
+  Home,
+  Body,
+  Div1,
+  Div2,
+  MemePara,
+  SubmitButton,
+} from './styledComponents'
+
+const fontSizesOptionsList = [
+  {
+    optionId: '8',
+    displayText: '8',
+  },
+  {
+    optionId: '12',
+    displayText: '12',
+  },
+  {
+    optionId: '16',
+    displayText: '16',
+  },
+  {
+    optionId: '20',
+    displayText: '20',
+  },
+  {
+    optionId: '24',
+    displayText: '24',
+  },
+  {
+    optionId: '28',
+    displayText: '28',
+  },
+  {
+    optionId: '32',
+    displayText: '32',
+  },
+]
+// Write your code here
+
+class MemeGenerator extends Component {
+  state = {
+    imgUrl: '',
+    topText: '',
+    bottomText: '',
+    fontSize: 8,
+    submitClicked: false,
+  }
+
+  onChangeFontSize = event => {
+    this.setState({fontSize: event.target.value})
+  }
+
+  changeUrl = event => {
+    this.setState({imgUrl: event.target.value})
+  }
+
+  render() {
+    const {fontSize, submitClicked} = this.state
+    return (
+      <>
+        <Home>
+          <Body>
+            <Div1>
+              <MainHeading>Meme Generator</MainHeading>
+              <form>
+                <SubHeading>Image URL</SubHeading>
+                <InputBox
+                  onChange={this.changeUrl}
+                  type="text"
+                  placeholder="Enter Image Url"
+                />
+
+                <SubHeading>Top Text</SubHeading>
+                <InputBox
+                  onChange={this.changeTopText}
+                  type="text"
+                  placeholder="Enter Top Text"
+                />
+
+                <SubHeading>Bottom Text</SubHeading>
+                <InputBox
+                  onChange={this.changeBottomText}
+                  type="text"
+                  placeholder="Enter Bottom Text"
+                />
+
+                <SubHeading>Font Size</SubHeading>
+                <InputBox
+                  value={fontSize}
+                  as="select"
+                  onChange={this.onChangeFontSize}
+                >
+                  {fontSizesOptionsList.map(each => (
+                    <option value={each.optionId}>{each.displayText}</option>
+                  ))}
+                </InputBox>
+                <SubmitButton type="submit">Generate</SubmitButton>
+              </form>
+            </Div1>
+            {submitClicked && (
+              <Div2>
+                <MemePara>dummy</MemePara>
+                <MemePara>dummy</MemePara>
+              </Div2>
+            )}
+          </Body>
+        </Home>
+      </>
+    )
+  }
+}
+
+export default MemeGenerator
---
+++ src/components/MemeGenerator/styledComponents.js
@@ -0,0 +1,50 @@
+// Style your components here
+import styled from 'styled-components'
+
+export const Home = styled.div`
+  height: 100vh;
+`
+
+export const Body = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+export const Div1 = styled.div`
+  width: 40%;
+`
+export const Div2 = styled.div`
+  width: 35%;
+`
+
+export const MainHeading = styled.h1`
+  color: #0b69ff;
+  font-family: Roboto;
+`
+
+export const SubHeading = styled.p`
+  color: grey;
+`
+export const InputBox = styled.input`
+  width: 100%;
+  padding-left: 10px;
+  height: 33px;
+  outline: none;
+`
+export const MemePara = styled.p`
+  color: white;
+`
+
+export const SubmitButton = styled.button`
+  width: 40%;
+  height: 33px;
+  background-color: #0b69ff;
+  color: white;
+  margin-top: 20px;
+  border-radius: 4px;
+  border: 0px;
+  font-size: 16px;
+  cursor: pointer;
+`
